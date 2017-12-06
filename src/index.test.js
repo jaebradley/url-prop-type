52697a5450e835623172cc92b7028ffe650ed3c2
jaebradley <jae.b.bradley@gmail.com>
2017-12-06 16:56:23 -0500

    feat(url-prop-type): validate relative paths
    fix(relative-url): use is-relative-url

--- src/index.test.js
+++ src/index.test.js
@@ -1,43 +1,70 @@
 
 import isUrl from 'is-url';
+import isRelativeUrl from 'is-relative-url';
 
 import urlPropType from './index';
 
 jest.mock('is-url', () => jest.fn());
+jest.mock('is-relative-url', () => jest.fn());
 
-describe('prop-types-url-validator', () => {
+describe('URL Prop Type', () => {
   const propName = 'baejadley';
   const componentName = 'jaebaebae';
   const url = 'http://google.com';
+  const nonStringUrl = 10;
   let mockedIsUrl;
+  let mockedIsRelativeUrl;
 
-  afterEach(() => mockedIsUrl.mockRestore());
+  afterEach(() => {
+    mockedIsUrl.mockRestore();
+    mockedIsRelativeUrl.mockRestore();
+  });
 
   describe('urlPropType', () => {
-    it('should throw an error if prop value is not a valid URL', () => {
+    it('should throw an error if prop value is not a string', () => {
+      mockedIsUrl = isUrl.mockImplementation(() => true);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => true);
+      const props = {};
+      props[propName] = nonStringUrl;
+      expect(urlPropType(props, propName, componentName))
+        .toEqual(new TypeError(`Invalid URL Prop Value: ${nonStringUrl} for ${propName} in ${componentName}`));
+    });
+
+    it('should throw an error if prop value is not a valid absolute URL nor a valid relative URL', () => {
       mockedIsUrl = isUrl.mockImplementation(() => false);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => false);
       const props = {};
       props[propName] = url;
       expect(urlPropType(props, propName, componentName))
         .toEqual(new TypeError(`Invalid URL Prop Value: ${url} for ${propName} in ${componentName}`));
     });
 
-    it('should return null if prop is not defined', () => {
+    it('should return null if prop value is a valid relative URL but not a valid absolute URL', () => {
+      mockedIsUrl = isUrl.mockImplementation(() => false);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => true);
       const props = {};
+      props[propName] = url;
       expect(urlPropType(props, propName, componentName)).toBeNull();
     });
 
-    it('should return null if prop value is a valid URL', () => {
+    it('should return null if prop value is a valid absolute URL but not a valid relative URL', () => {
       mockedIsUrl = isUrl.mockImplementation(() => true);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => false);
       const props = {};
       props[propName] = url;
+      expect(urlPropType(props, propName, componentName)).toBeNull();
+    });
+
+    it('should return null if prop is not defined', () => {
+      const props = {};
       expect(urlPropType(props, propName, componentName)).toBeNull();
     });
   });
 
   describe('validateRequiredUrl', () => {
-    it('should throw an error if prop value is not a valid URL', () => {
+    it('should throw an error if prop value is not a valid absolute URL nor a valid relative URL', () => {
       mockedIsUrl = isUrl.mockImplementation(() => false);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => false);
       const props = {};
       props[propName] = url;
       expect(urlPropType.isRequired(props, propName, componentName))
@@ -50,8 +77,17 @@ describe('prop-types-url-validator', () => {
         .toEqual(new TypeError(`Invalid URL Prop Value: undefined for ${propName} in ${componentName}`));
     });
 
-    it('should return null if prop value is a valid URL', () => {
+    it('should return null if prop value is a valid relative URL but not a valid absolute URL', () => {
+      mockedIsUrl = isUrl.mockImplementation(() => false);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => true);
+      const props = {};
+      props[propName] = url;
+      expect(urlPropType.isRequired(props, propName, componentName)).toBeNull();
+    });
+
+    it('should return null if prop value is a valid absolute URL but not a valid relative URL', () => {
       mockedIsUrl = isUrl.mockImplementation(() => true);
+      mockedIsRelativeUrl = isRelativeUrl.mockImplementation(() => false);
       const props = {};
       props[propName] = url;
       expect(urlPropType.isRequired(props, propName, componentName)).toBeNull();
